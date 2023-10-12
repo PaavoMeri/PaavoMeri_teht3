7d5a7037ab39593f816c604dd12275250dfabb8c
PaavoMeri <97727217+PaavoMeri@users.noreply.github.com>
2023-10-12 15:14:03 +0300

    Update settings.py

--- tehtävä3/Tehtävä_3/settings.py
+++ tehtävä3/Tehtävä_3/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k4rcqu^au&nwjw9-6vu%mhn2g*!77od-ytm)^p@lxjm92y4k@#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
